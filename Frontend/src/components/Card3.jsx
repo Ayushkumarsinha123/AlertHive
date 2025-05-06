034261da65bcdccc90dc22db5d21f863a2f43763
alok <alok.kr300@gmail.com>
2025-05-06 19:43:40 +0530

    Added border to live feed

--- Frontend/src/components/Card3.jsx
+++ Frontend/src/components/Card3.jsx
@@ -20,12 +20,12 @@ export default function Card3({ title }) {
       {title && (
         <h2 className="text-md font-semibold text-gray-800 mb-2">{title}</h2>
       )}
-      <div className="flex-grow space-y-3 overflow-y-auto pr-1">
+      <div className="flex-grow space-y-3 overflow-y-auto pr-1 ">
         {dummyContent.map((item, idx) => (
           <a
             key={idx}
             href={item.link}
-            className="flex items-center gap-2 bg-white rounded-lg p-2 hover:shadow transition duration-200"
+            className="flex items-center gap-2 bg-white rounded-lg p-2 hover:shadow transition duration-200 border border-[#ddd]"
           >
             {sourceIcons[item.source]}
             <span className="text-sm text-gray-700">{item.title}</span>
