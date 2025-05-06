16939588c58b2f9d31b5cdd8aaffed1c240fff41
alok <alok.kr300@gmail.com>
2025-05-07 05:29:14 +0530

    Wrapped ws context to App

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,16 +3,19 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/DashboardPage.jsx';
 import Reports from './pages/Reports';
 import InsightsDashboard from './pages/AdminDashboard.jsx';
+import { WebSocketProvider } from './contexts/WebSocketContext.jsx';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/reports" element={<Reports />} />
-        <Route path="/admin" element={<InsightsDashboard/>}/>
-      </Routes>
-    </Router>
+    <WebSocketProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/reports" element={<Reports />} />
+          <Route path="/admin" element={<InsightsDashboard />} />
+        </Routes>
+      </Router>
+    </WebSocketProvider>
   );
 }
 
