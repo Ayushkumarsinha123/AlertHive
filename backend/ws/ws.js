40122a8060610c4a01582a99459d5a232c3527c6
alok <alok.kr300@gmail.com>
2025-05-07 05:27:28 +0530

    Calling runWorker from ws.js

--- backend/ws/ws.js
+++ backend/ws/ws.js
@@ -1,6 +1,7 @@
 const { setupWebSocketServer } = require("./server");
 const { createServer } = require("http");
 const express = require("express");
+const runWorker = require('../scheduler/customFetchAPI');
 
 const app = express();
 const server = createServer(app);
@@ -9,4 +10,8 @@ setupWebSocketServer(server);
 
 server.listen(9000, () => {
   console.log("Listening on http://localhost:9000");
+
+  // Running the WORKER here ( if scheduler runs in different node process)
+  // then memory will be different, and fetching data will be an issue
+  runWorker()
 });
